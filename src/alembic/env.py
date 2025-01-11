25f2c440b7d901100996d93eb404d66581e83307
SanjarbekAI <sanjarbekwork@gmail.com>
2025-01-11 17:34:37 +0500

    file structure is changed

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from src.config.env_variables import DATABASE_URL
-from src.config.models import metadata
+from src.config.db.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
